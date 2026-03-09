a48ba0df3005865620a2e605502129896a340580
Sainanigans <149588146+Sainanigans@users.noreply.github.com>
2026-03-09 17:53:01 +0100

    Gold Route rework
    Rework and adjustment of the Gold Route, including .chain and the material files. It is all still a closed loop but the chemistry is easier to follow and more based on reality.

--- src/main/java/gregicadditions/recipes/chain/GoldChain.java
+++ src/main/java/gregicadditions/recipes/chain/GoldChain.java
@@ -14,33 +14,68 @@ public class GoldChain {
 
     public static void init() {
 
-        /* Gold Chain Attempt #4
+        /* Gold Chain Attempt #5
          *
-         * This chain has undergone a very careful re-balance to make it more rewarding and less intimidating.
+         * A rework of the already existing (and honestly quite accurate despite the flaws) gold processing chain,
+         * trying to preserve it as much as possible while replicating real processes and chemistry.
          *
-         * The main stages of the chain:
+         * The chain features a total of 5 routes, each with increasing complexity, machines required and gold yield.
+         * All elements are fully conserved and recycled with the exception of gold, which varies from one to another 
+         * to incentive the pursue of longer routes.
          *
-         * - Precious Metal Ingot: Smelts to 1 nugget of gold
-         * - Gold Alloy: Centrifuges to 1 nugget of gold each, 4 nuggets total per Precious Metal
-         * - Gold Leach: Electrolyzes to 2 nuggets of gold each, 8 nuggets total per Precious Metal
-         * - Chloroauric Acid: The step creating this outputs the byproducts, and returns the Copper from Gold Alloy
-         * - Chloroauric Acid: Processes into 2 ingots
+         * ROUTES:
+         * - Route 1: Direct smelting of 1 Precious Metal Ingot to 1 gold nugget - Yield: 1 ngt per 1 PM ingot
+         * - Route 2: Gold Alloy centrifugation to 1 gold nugget each - Yield: 4 ngt per 1 PM ingot
+         * - Route 3: Gold Leach electrolysis to 2 gold nuggets each - Yield: 8 ngt per 1 PM ingot
+         * - Route 4: Chloroauric Acid decomposition to 6 gold nuggets each - Yield: 12 ngt per 1 PM ingot
+         * - Route 5: Chloroauric Acid electrolysis to 9 gold nuggets each - Yield: 18 ngt per 1 PM ingot
          *
-         * In the end, each step has a major compensation, but each step is reasonable return.
-         * STEP 1: +3 nuggets of gold
-         * STEP 2: +4 nuggets of gold
-         * STEP 3: +byproducts and Copper no longer voided
-         * STEP 4: +10 nuggets of gold
+         * In adittion to the gold, routes 2 to 5 also allow the recovery of all the copper invested. Routes 4 and 5 also
+         * yield adittional metals as byproduct.
          *
-         * Everything else in this chain is a fully closed loop.
+         * For reaction purposes, the chemical formulas and units/mol involved are:
+         * - Precious Metal Ingot: Au2? - 1 unit/mol
+         * - Gold Alloy: Cu3Au2? - 4 units/mol 
+         * - Gold Leach: Cu3Au2(OH)3? - 4 units/mol
+         * - Chloroauric Acid: HAuCl4·3H2O - 1 unit/mol
+         * - Copper Leach: Cu3? - 4 unit/mol
          */
 
-        // RECOVERY STEPS ==============================================================================================
-
-        // Step 0 recovery (1 nugget per PM)
+        // ROUTES and REACTIONS ==============================================================================================
+        // ROUTE 1
+        // Step 1.1 - Smelting
+        // 1 Precious Metal Ingot -> 1 Gold Nugget
         ModHandler.addSmeltingRecipe(OreDictUnifier.get(ingot, PreciousMetal), OreDictUnifier.get(nugget, Gold));
 
-        // Step 1 recovery (8 nuggets per PM)
+        // ROUTE 2
+        // Step 2.1 - Gold alloy with Copper - Different variants for ingot and dust form for both
+        // Au2? + 3Cu -> Cu3Au2?
+        ALLOY_SMELTER_RECIPES.recipeBuilder().EUt(30).duration(100)
+                .input(dust, PreciousMetal)
+                .input(dust, Copper, 3)
+                .output(ingot, GoldAlloy, 4)
+                .buildAndRegister();
+
+        ALLOY_SMELTER_RECIPES.recipeBuilder().EUt(30).duration(100)
+                .input(ingot, PreciousMetal)
+                .input(dust, Copper, 3)
+                .output(ingot, GoldAlloy, 4)
+                .buildAndRegister();
+
+        ALLOY_SMELTER_RECIPES.recipeBuilder().EUt(30).duration(100)
+                .input(dust, PreciousMetal)
+                .input(ingot, Copper, 3)
+                .output(ingot, GoldAlloy, 4)
+                .buildAndRegister();
+
+        ALLOY_SMELTER_RECIPES.recipeBuilder().EUt(30).duration(100)
+                .input(ingot, PreciousMetal)
+                .input(ingot, Copper, 3)
+                .output(ingot, GoldAlloy, 4)
+                .buildAndRegister();
+
+        // Step 2.2 - Gold Alloy centrifugation
+        // Cu3Au2? -> 3Cu + 2 Au (4 ngt)
         CENTRIFUGE_RECIPES.recipeBuilder()
                 .input(dust, GoldAlloy, 4)
                 .output(dust, Copper, 3)
@@ -49,18 +84,70 @@ public class GoldChain {
                 .EUt(30)
                 .buildAndRegister();
 
-        // Step 2 recovery (16 nuggets per PM)
+        // ROUTE 3
+        // Step 3.1 - Gold oxidation to gold hydroxide Au(OH)3
+        // Cu3Au? + 6 HNO3 -> Cu3Au2(OH)3? + 6 NO2
+        CHEMICAL_RECIPES.recipeBuilder().duration(80)
+                .input(ingot, GoldAlloy, 4)
+                .fluidInputs(NitricAcid.getFluid(6000))
+                .outputs(GoldLeach.getItemStack(4))
+                .fluidOutputs(NitrogenDioxide.getFluid(6000))
+                .buildAndRegister();
+
+        // Step 3.2 - Gold hydroxide neutralization
+        // Cu3Au2(OH)3? + 8 HCl -> 2 HAuCl4·3H2O + Cu3?
+        CHEMICAL_RECIPES.recipeBuilder().duration(80)
+                .inputs(GoldLeach.getItemStack(4))
+                .fluidInputs(HydrochloricAcid.getFluid(8000))
+                .outputs(CopperLeach.getItemStack(4))
+                .fluidOutputs(ChloroauricAcid.getFluid(2000))
+                .buildAndRegister();
+
+        // Step 3.3 - Gold Leach electrolysis
+        // Cu3Au2(OH)3? + 3 H2 -> 6 H2O + 3 Cu + 2 Au (8 ngt)
         ELECTROLYZER_RECIPES.recipeBuilder()
                 .inputs(GoldLeach.getItemStack(4))
-                .fluidInputs(Hydrogen.getFluid(1000))
-                .fluidOutputs(Water.getFluid(1000))
+                .fluidInputs(Hydrogen.getFluid(6000))
+                .fluidOutputs(Water.getFluid(6000))
                 .output(dust, Copper, 3)
                 .output(dustTiny, Gold, 8)
                 .duration(300)
                 .EUt(30)
                 .buildAndRegister();
 
-        // Step 3 recovery
+        // ROUTE 4
+        // Step 4.0 - Potassium metabisulfite synthesis (for the starting dust)
+        // 6 K + 6 S + 15/2 O2 -> 3 K2S2O5
+        MIXER_RECIPES.recipeBuilder().duration(100).EUt(30)
+                .notConsumable(new IntCircuitIngredient(1))
+                .input(dust, Potassium, 2)
+                .input(dust, Sulfur, 2)
+                .fluidInputs(Oxygen.getFluid(5000))
+                .output(dust, PotassiumMetabisulfite, 9)
+                .buildAndRegister();
+
+        // Step 4.1 - Chloroauric acid decomposition with K2S2O5
+        // 4 HAuCl4·3H20 -> 16 HCl + 3 O2 + 6 H2O + 4 Au (6 ngt)
+        // This reaction is a bit tricky.  The chloroauric acid decomposition is usually done with SO2 or with an agent that
+        // liberates it by hydrolysis (like K2S2O5). However, the metabisulfite is not a calatyst but a reducing agent; the
+        // reactions involved on its regeneration would require the same number of steps as the final route, so to keep it cheaper
+        // in order to justify the lower yield -as the gold purity is lower- the K2S2O5 usage and regeneration has been compressed
+        // into a single reaction:
+        // Reaction 1: Chloroauric decomposition: 4 HAuCl4·3H2O + 3 K2S2O5 -> 4 Au + 16 HCl + 6 KHSO4 + 3 H2O
+        // Reaction 2: Potassion bisulfite electrolysis: 6 KHSO4 -> 6 K + 3 H2 + 6 S + 12 O2 -unreallistic-
+        // Reaction 3: Water regeneration: 6 H + 3 O -> 3 H2O
+        // Reaction 4: Potassion metabisulfite regeneration: 6 K + 6 S + 15/2 O2 -> 3 K2S2O5
+        // Total reaction: 4 HAuCl4·3H2O -> 4 Au + 16 HCl + 3 O2 + 6 H2O
+        CHEMICAL_RECIPES.recipeBuilder().duration(100)
+                .fluidInputs(ChloroauricAcid.getFluid(2000))
+                .notConsumable(dust, PotassiumMetabisulfite)
+                .output(dustTiny, Gold, 6)
+                .fluidOutputs(HydrochloricAcid.getFluid(8000))
+                .fluidOutputs(Water.getFluid(3000))
+                .fluidOutputs(Oxygen.getFluid(3000))
+                .buildAndRegister();
+
+        // Step 4.2 - Copper Leach decomposition
         // This step does not directly process Chloroauric Acid, and instead is processing
         // other byproducts from the chain, which are compacted from the older versions of the chain.
         // Cu3? -> 3Cu + Fe + Ni + Ag + Pb
@@ -73,72 +160,18 @@ public class GoldChain {
                 .chancedOutput(OreDictUnifier.get(dust, Silver), 800, 200)
                 .buildAndRegister();
 
-
-        // MAIN CHAIN ==================================================================================================
-
-        // STEP 1
-        // Au? + 3Cu -> Cu3Au?
-        ALLOY_SMELTER_RECIPES.recipeBuilder().EUt(30).duration(100)
-                .input(dust, PreciousMetal)
-                .input(dust, Copper, 3)
-                .output(ingot, GoldAlloy, 4)
-                .buildAndRegister();
-
-        ALLOY_SMELTER_RECIPES.recipeBuilder().EUt(30).duration(100)
-                .input(ingot, PreciousMetal)
-                .input(dust, Copper, 3)
-                .output(ingot, GoldAlloy, 4)
-                .buildAndRegister();
-
-        ALLOY_SMELTER_RECIPES.recipeBuilder().EUt(30).duration(100)
-                .input(dust, PreciousMetal)
-                .input(ingot, Copper, 3)
-                .output(ingot, GoldAlloy, 4)
-                .buildAndRegister();
-
-        ALLOY_SMELTER_RECIPES.recipeBuilder().EUt(30).duration(100)
-                .input(ingot, PreciousMetal)
-                .input(ingot, Copper, 3)
-                .output(ingot, GoldAlloy, 4)
-                .buildAndRegister();
-
-        // STEP 2
-        // Cu3Au? + HNO3 -> Cu3Au?(OH) + NO2
-        CHEMICAL_RECIPES.recipeBuilder().duration(80)
-                .input(ingot, GoldAlloy, 4)
-                .fluidInputs(NitricAcid.getFluid(1000))
-                .outputs(GoldLeach.getItemStack(4))
-                .fluidOutputs(NitrogenDioxide.getFluid(1000))
-                .buildAndRegister();
-
-        // STEP 3
-        // Cu3Au?(OH) + HCl -> HAuCl(OH) + Cu3?
-        CHEMICAL_RECIPES.recipeBuilder().duration(80)
-                .inputs(GoldLeach.getItemStack(4))
-                .fluidInputs(HydrochloricAcid.getFluid(1000))
-                .outputs(CopperLeach.getItemStack(4))
-                .fluidOutputs(ChloroauricAcid.getFluid(1000))
-                .buildAndRegister();
-
-        // STEP 4
-        // HAuCl(OH) -> Au + H2O + Cl
-        CHEMICAL_RECIPES.recipeBuilder().duration(100)
+        // ROUTE 5
+        // Step 5.1 - Chloroauric Acid electrolysis - Mimicking the Wohlwill process
+        // 2 HAuCl4·3H2O -> 2Au + 2 HCl + 3 Cl2 + 6 H2O
+        ELECTROLYZER_RECIPES.recipeBuilder()
                 .fluidInputs(ChloroauricAcid.getFluid(1000))
-                .notConsumable(dust, PotassiumMetabisulfite)
                 .output(dust, Gold, 2)
-                .fluidOutputs(Water.getFluid(1000))
-                .fluidOutputs(Chlorine.getFluid(1000))
+                .fluidOutputs(Water.getFluid(3000))
+                .fluidOutputs(Chlorine.getFluid(3000))
+                .fluidOutputs(HydrochloricAcid.getFluid(1000))
+                .duration(100)
+                .EUt(30)
                 .buildAndRegister();
-
-        // SIDE INGREDIENTS ============================================================================================
-
-        // NOT CONSUMED INGREDIENT
-        MIXER_RECIPES.recipeBuilder().duration(100).EUt(30)
-                .notConsumable(new IntCircuitIngredient(1))
-                .input(dust, Potassium, 2)
-                .input(dust, Sulfur, 2)
-                .fluidInputs(Oxygen.getFluid(5000))
-                .output(dust, PotassiumMetabisulfite, 9)
-                .buildAndRegister();
+        
     }
 }
